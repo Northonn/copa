0812572c4f052b48d4f36e4919c4562f9b7d8fe6
Northonn Oliveira <northonnoliveira@Mac-mini-de-Northonn.local>
2026-06-29 09:39:11 -0300

    Add phase scoring summary

---
+++ src/officialResults.ts
@@ -0,0 +1,42 @@
+export const officialWinners: Record<number, string | undefined> = {
+  // 16 avos
+  73: 'can',
+  74: undefined,
+  75: undefined,
+  76: undefined,
+  77: undefined,
+  78: undefined,
+  79: undefined,
+  80: undefined,
+  81: undefined,
+  82: undefined,
+  83: undefined,
+  84: undefined,
+  85: undefined,
+  86: undefined,
+  87: undefined,
+  88: undefined,
+
+  // Oitavas
+  89: undefined,
+  90: undefined,
+  91: undefined,
+  92: undefined,
+  93: undefined,
+  94: undefined,
+  95: undefined,
+  96: undefined,
+
+  // Quartas
+  97: undefined,
+  98: undefined,
+  99: undefined,
+  100: undefined,
+
+  // Semifinais
+  101: undefined,
+  102: undefined,
+
+  // Final
+  104: undefined,
+}
